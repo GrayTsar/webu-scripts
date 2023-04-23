81858e92f6242e8a1e9215ef39bfcb929a3a6392
Slav <viatcheslav.ehrmann@gmail.com>
2023-04-23 15:28:16 +0200

    add icon and index for panda novel

--- scripts/panda-novel.lua
+++ scripts/panda-novel.lua
@@ -1,23 +1,23 @@
 function getChapterText(url)
     local request = lib:getRequestBuilder():url(url):addHeader("referer", url):build()
     local result = lib:executeRequest(request, 'https://www.panda-novel.com')
-    local textResult = result:selectFirst('div#novelArticle2'):select('p')
-	return textResult:toString()
+    local text = result:selectFirst('div#novelArticle2'):select('p')
+	return text:toString()
 end
 
 function search(searchQuery)
-    local query = searchQuery:gsub(' ', '%20')
-    local searchUrl = 'https://www.panda-novel.com/search/' .. query
-    local document = lib:getDocument(searchUrl)
-    local result = document:select('li.novel-li')
+    local query = searchQuery:gsub('%s', '%%20')
+    local url = 'https://www.panda-novel.com/search/' .. query
+    local doc = lib:getDocument(url)
+    local res = doc:select('li.novel-li')
 
     local list = lib:createWebsiteSearchList()
-	local count = result:size()
-	if(count > 0) then
-		for i=0,count-1,1 do
-			local link = result:get(i):selectFirst('a[href]'):attr('abs:href')
-			local title = result:get(i):selectFirst('div.novel-desc'):child(0):text()
-			local imgSrc = result:get(i):selectFirst('div.novel-cover'):child(0):attr('v-lazy:background-image'):gsub('\'', '')
+	local size = res:size()
+	if(size > 0) then
+		for i=0,size-1,1 do
+			local link = res:get(i):selectFirst('a[href]'):attr('abs:href')
+			local title = res:get(i):selectFirst('div.novel-desc'):child(0):text()
+			local imgSrc = res:get(i):selectFirst('div.novel-cover'):child(0):attr('v-lazy:background-image'):gsub('\'', '')
 			lib:addWebsiteSearchToList(list, link, title, imgSrc)
 		end
 	end
@@ -25,25 +25,24 @@ function search(searchQuery)
 end
 
 function parseNovel(url)
-	local novelDoc = lib:getDocument(url)
-	local webNovel = lib:createWebsiteNovel()
+	local doc = lib:getDocument(url)
+	local novel = lib:createWebsiteNovel()
 	local id = url:match('(%d+)$')
-	local capi = 'https://www.panda-novel.com/api/book/chapters/' .. id
+	local api = 'https://www.panda-novel.com/api/book/chapters/' .. id
 
-	webNovel:setTitle(novelDoc:selectFirst('div.novel-desc'):child(0):text())
-	webNovel:setImageUrl(novelDoc:selectFirst('meta[property=og:image]'):attr("content"))
-	webNovel:setDescription(novelDoc:selectFirst("div.synopsis-content"):select("p"):text())
-	webNovel:setAuthor(novelDoc:selectFirst('div.novel-desc'):child(1):child(1):text())
-	webNovel:setGenres(novelDoc:selectFirst('div.novel-labels'):children():textNodes():toString():gsub('[%[%]]', ''))
-	webNovel:setTags(novelDoc:selectFirst('ul.tags-list'):children():eachText():toString():gsub('[%[%]]', ''))
-	webNovel:setStatus(novelDoc:select('ul.novel-labs'):get(1):child(1):child(0):text())
+	novel:setTitle(doc:selectFirst('div.novel-desc'):child(0):text())
+	novel:setImageUrl(doc:selectFirst('meta[property=og:image]'):attr("content"))
+	novel:setDescription(doc:selectFirst("div.synopsis-content"):select("p"):text())
+	novel:setAuthor(doc:selectFirst('div.novel-desc'):child(1):child(1):text())
+	novel:setGenres(doc:selectFirst('div.novel-labels'):children():textNodes():toString():gsub('[%[%]]', ''))
+	novel:setTags(doc:selectFirst('ul.tags-list'):children():eachText():toString():gsub('[%[%]]', ''))
+	novel:setStatus(doc:select('ul.novel-labs'):get(1):child(1):child(0):text())
 
-	local chapterList = lib:createWebsiteChapterList()
-
+	local chapters = lib:createWebsiteChapterList()
 	local hasNext = 1
 	local cPage = 1
 	while hasNext==1 do
-		local cReq = lib:getRequestBuilder():url(capi .. '/' .. cPage):addHeader("referer", url .. '/chapters'):build()
+		local cReq = lib:getRequestBuilder():url(api .. '/' .. cPage .. '?_=' .. (os.time() * 1000)):addHeader("referer", url .. '/chapters'):build()
 		local cDoc = lib:executeRequest(cReq, 'https://www.panda-novel.com')
 		local tree = lib:toJsonTree(cDoc:text())
 		local data = lib:getFromJsonObject(tree, 'data')
@@ -57,7 +56,7 @@ function parseNovel(url)
 			local item = lib:elementAsObject(element)
 			local link = 'https://www.panda-novel.com/' .. item:get('chapterUrl'):getAsString()
 			local title = item:get("name"):getAsString()
-			lib:addWebsiteChaptersToList(chapterList, link, title, '')
+			lib:addWebsiteChaptersToList(chapters, link, title, '')
 		end
 
 		if cPage+1 > pages then
@@ -66,8 +65,6 @@ function parseNovel(url)
 			cPage = cPage + 1
 		end
 	end
-
-	webNovel:setChapters(chapterList)
-
-	return webNovel
+	novel:setChapters(chapters)
+	return novel
 end
